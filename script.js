04f754dbfb89d502eead2266b4841437f0d800ff
nafij-islam <sahariannafis70@gmail.com>
2025-05-07 23:03:06 +0600

    update

---
+++ script.js
@@ -0,0 +1,22 @@
+//variable 
+
+
+
+
+// let x= 20;
+
+// let y= 30;
+
+
+
+// if (x>=20) {
+//     console.log("how are you")
+    
+// }else {
+//     console.log("what")
+// }
+
+
+
+console.log("10")
+
