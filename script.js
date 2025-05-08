2ad399b87f265c60c0d5c1c8bc3656818beabc66
nafij-islam <sahariannafis70@gmail.com>
2025-05-08 23:22:16 +0600

    update

--- script.js
+++ script.js
@@ -18,5 +18,48 @@
 
 
 
-console.log("10")
+
+// console.log('hello world');
+
+// document.getElementById('title').innerHTML="how are you"
+
+// let sum= 40+50;
+
+// console.log(sum);
+
+
+
+// let x=200;
+// let y=100;
+
+// if (x<300){
+//     console.log('hi')
+// }else{
+//     console.log('what')
+// }
+
+
+
+let x=50;
+
+if (x>=40){
+    console.log('boro')
+}else{
+    console.log('chuto')
+}
+
+
+
+
+let sa = '01633003462';
+
+
+if (sa<='016330034'){
+    
+    console.log('right');
+
+
+} else{
+    console.log('what')
+}
 
