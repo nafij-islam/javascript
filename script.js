53e235bc39a39b6fe59b927f65d8653704ad0c29
nafij-islam <sahariannafis70@gmail.com>
2025-05-12 11:26:59 +0600

    update

--- script.js
+++ script.js
@@ -1,74 +1,100 @@
-//variable 
+// //variable 
 
 
 
 
-// let x= 20;
+// // let x= 20;
 
-// let y= 30;
+// // let y= 30;
 
 
 
-// if (x>=20) {
-//     console.log("how are you")
+// // if (x>=20) {
+// //     console.log("how are you")
     
-// }else {
-//     console.log("what")
+// // }else {
+// //     console.log("what")
+// // }
+
+
+
+
+// // console.log('hello world');
+
+// // document.getElementById('title').innerHTML="how are you"
+
+// // let sum= 40+50;
+
+// // console.log(sum);
+
+
+
+// // let x=200;
+// // let y=100;
+
+// // if (x<300){
+// //     console.log('hi')
+// // }else{
+// //     console.log('what')
+// // }
+
+
+
+// let x=50;
+
+// if (x>=40){
+//     console.log('boro')
+// }else{
+//     console.log('chuto')
 // }
 
 
 
 
-// console.log('hello world');
-
-// document.getElementById('title').innerHTML="how are you"
-
-// let sum= 40+50;
-
-// console.log(sum);
+// let sa = '01633003462';
 
 
+// if (sa<='016330034'){
+    
+//     console.log('right');
 
-// let x=200;
-// let y=100;
 
-// if (x<300){
-//     console.log('hi')
-// }else{
+// } else{
 //     console.log('what')
 // }
 
 
+// let s = 'saharian';
 
-let x=50;
+// if (s==='sahrian'){
+//     console.log('hi')
+// } else{
+//     console.log('no')
+    
+// }
 
-if (x>=40){
-    console.log('boro')
+let age = 17;
+
+let country = 'brazil';
+
+let gender = 'mail';
+
+
+if (age>=17){
+    console.log("next stap")
 }else{
-    console.log('chuto')
+    console.log("error")
 }
 
-
-
-
-let sa = '01633003462';
-
-
-if (sa<='016330034'){
-    
-    console.log('right');
-
-
-} else{
-    console.log('what')
+if (country == "brazil"){
+    console.log('next stap')
+}else{
+    console.log("error")
 }
 
+if(gender == "mail"){
+    console.log("next stap")
 
-let s = 'saharian';
-
-if (s==='sahrian'){
-    console.log('hi')
-} else{
-    console.log('no')
-    
+}else{
+    console.log("error")
 }
