92086be494be2e09b5f72e828e1ca5413e0f9f89
nafij-islam <sahariannafis70@gmail.com>
2025-05-20 22:08:05 +0600

    update

--- script.js
+++ script.js
@@ -1,165 +1,165 @@
-// variable 
-// COndition
-// Loop
+// // variable 
+// // COndition
+// // Loop
 
 
 
 
-/*
-*Variable
+// /*
+// *Variable
 
-Let  FirstName = "Nafis";
+// Let  FirstName = "Nafis";
 
-!Let ---       Variable datar location dore rhakar akta store
+// !Let ---       Variable datar location dore rhakar akta store
 
-!FirstName---  Variable ke chinar jonno/verify korar jonno akta 
+// !FirstName---  Variable ke chinar jonno/verify korar jonno akta 
 
-!"NAfis"---    variable  ar Man/value
+// !"NAfis"---    variable  ar Man/value
 
-*/
-
-
-/*========================
-        Condition
- =========================*/
-
-/*
+// */
 
 
-                     !Condition
+// /*========================
+//         Condition
+//  =========================*/
+
+// /*
 
 
-* Condition Ar mane Hocce === Jodi tar result GPA 5 hoy tahole she
-
-?print
-!Console.log("Pass")
+//                      !Condition
 
 
-*jodi tar result  GPA 5 ar nise thake tahole 
+// * Condition Ar mane Hocce === Jodi tar result GPA 5 hoy tahole she
 
-!Console.log("Fail")
-
-?  if (Store GPA5 == student GPA 5  ) jodi pass hoy tahole
-
-?print
-
-?{Console.log("pass")}
+// ?print
+// !Console.log("Pass")
 
 
+// *jodi tar result  GPA 5 ar nise thake tahole 
+
+// !Console.log("Fail")
+
+// ?  if (Store GPA5 == student GPA 5  ) jodi pass hoy tahole
+
+// ?print
+
+// ?{Console.log("pass")}
 
 
 
-^Condition
+
+
+// ^Condition
 
 
 
-*/
+// */
 
 
 
-let x= 20;
+// let x= 20;
 
-let y= 30;
+// let y= 30;
 
 
 
-if (x>=20) {
-    console.log("how are you")
+// if (x>=20) {
+//     console.log("how are you")
     
-}else {
-    console.log("what")
-}
+// }else {
+//     console.log("what")
+// }
 
 
 
 
-console.log('hello world');
+// console.log('hello world');
 
-document.getElementById('title').innerHTML="how are you"
+// document.getElementById('title').innerHTML="how are you"
 
-let sum= 40+50;
+// let sum= 40+50;
 
-console.log(sum);
-
-
+// console.log(sum);
 
 
-let sa = '01633003462';
-let salary = 20000;
-let ag = 20;
 
 
-if (sa<='01633003462'){
-    if(salary > 15000){
-        if(ag>=19){
-            console.log("ag thik acha")
-        }else{
-            console.log("ag aro baraw")
-        }
-    }else{
-        console.log("biya korte parba na")
-    }
+// let sa = '01633003462';
+// let salary = 20000;
+// let ag = 20;
+
+
+// if (sa<='01633003462'){
+//     if(salary > 15000){
+//         if(ag>=19){
+//             console.log("ag thik acha")
+//         }else{
+//             console.log("ag aro baraw")
+//         }
+//     }else{
+//         console.log("biya korte parba na")
+//     }
     
     
-}else{
-    console.log('what')
-}
+// }else{
+//     console.log('what')
+// }
 
 
-let s = 'saharian';
+// let s = 'saharian';
 
-if (s==='sahrian'){
-    console.log('hi')
-} else{
-    console.log('no')
+// if (s==='sahrian'){
+//     console.log('hi')
+// } else{
+//     console.log('no')
     
-}
+// }
 
-// football macth condition
+// // football macth condition
 
-let age = 17;
-let country = "brazil";
-let gender = "mail";
-let height = 5;
-let experience = true;
-let medicalreport = "pass";
-let ifdraguser = false;
-let traningYears = 1;
-let criminalRecord = "none";
-let english = "good";
+// let age = 17;
+// let country = "brazil";
+// let gender = "mail";
+// let height = 5;
+// let experience = true;
+// let medicalreport = "pass";
+// let ifdraguser = false;
+// let traningYears = 1;
+// let criminalRecord = "none";
+// let english = "good";
 
 
-if ( age >= 17 &&
-     country =="brazil" &&
-     gender == "mail" &&
-     height <=  5 &&
-     experience == true &&
-     medicalreport == "pass" &&
-     ifdraguser == false &&
-     traningYears <=2 &&
-     criminalRecord == "none" &&
-     english == "good"
+// if ( age >= 17 &&
+//      country =="brazil" &&
+//      gender == "mail" &&
+//      height <=  5 &&
+//      experience == true &&
+//      medicalreport == "pass" &&
+//      ifdraguser == false &&
+//      traningYears <=2 &&
+//      criminalRecord == "none" &&
+//      english == "good"
      
-){
-    console.log("pass")
-}else{
-    console.log("false")
-}
+// ){
+//     console.log("pass")
+// }else{
+//     console.log("false")
+// }
 
 
 
 
-// 
+// // 
 
 
 
-var aa = 'nafis islam';
+// var aa = 'nafis islam';
 
-if (aa =='nafis islam'){
-    console.log('hello')
-}
+// if (aa =='nafis islam'){
+//     console.log('hello')
+// }
 
-// aaaaaaaaaaaaaaaaaaaaaaaa
+// // aaaaaaaaaaaaaaaaaaaaaaaa
 
 
 let a = 2;
@@ -176,68 +176,68 @@ if (a== 2 &&
 
 
  
-/*========================
-         loop
-==========================*/
+// /*========================
+//          loop
+// ==========================*/
 
-/*
+// /*
 
-*LOOP Ar Mane hocce ja kunu akta Content ba string/number Repeat kora /  bar bar dekanu 
-
-
-*/
-
-// let u = "My Name is Saharian Nafis";
+// *LOOP Ar Mane hocce ja kunu akta Content ba string/number Repeat kora /  bar bar dekanu 
 
 
+// */
+
+// // let u = "My Name is Saharian Nafis";
 
 
 
 
-/*
-*2 X 1 = 2
-*2 X 2 = 4
-*/
-
-/*
-!Loop
-
-*for(Intialvalue; Condition; iteration) 
-*/
 
 
-/*
+// /*
+// *2 X 1 = 2
+// *2 X 2 = 4
+// */
 
-let namta=67
+// /*
+// !Loop
 
-for(let i=1; i<=10;i++){
-     console.log(namta+" X "+i+" = "+ namta*i);
-}
-
-
-*/
+// *for(Intialvalue; Condition; iteration) 
+// */
 
 
+// /*
 
-/*
-*2 X 1 = 2
-*2 X 2 = 4
-*/
+// let namta=67
 
-let namta = 20
+// for(let i=1; i<=10;i++){
+//      console.log(namta+" X "+i+" = "+ namta*i);
+// }
 
-for (let  i=1; i<=10; i++) {
-    if(i==5){
-        console.log("this is numver five")
-    }
+
+// */
+
+
+
+// /*
+// *2 X 1 = 2
+// *2 X 2 = 4
+// */
+
+// let namta = 20
+
+// for (let  i=1; i<=10; i++) {
+//     if(i==5){
+//         console.log("this is numver five")
+//     }
     
-}
+// }
 
 
 
-/*
-* while loop
-*/
+// /*
+// * while loop
+// */
 
 let i=0
 
@@ -249,14 +249,33 @@ while(i<10){
 
 
 
-/*
-* do while loop
-*/
+// /*
+// * do while loop
+// */
 
 
-let u=0
+// let u=0
 
-do{
-    console.log("how are you",u);
-    u++
-}while(u<10)
+// do{
+//     console.log("how are you",u);
+//     u++
+// }while(u<10)
+
+//     if
+
+
+
+
+
+for(let i=1; i<5;i++){
+    if(i>4){
+        console.log("this is four");
+    }
+    console.log("this is java script nasted loop",i);
+    for(let j=1; j<=10;j++){
+        if(j>10){
+            console.log("thi is");
+        }
+        console.log("this is nasted",j);
+    }
+}
